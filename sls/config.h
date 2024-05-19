09be686f5675e2ea5713f6b36a3632b2ca49c380
hoangloiQ <ndeav351@gmail.com>
2024-05-19 21:40:17 +0700

    Add files via upload

---
+++ sls/config.h
@@ -0,0 +1,27 @@
+/* slstatus configuration */
+/* See LICENSE file for copyright and license details. */
+
+/* interval between updates (in ms) */
+const unsigned int interval = 1000;
+
+/* text to show if no value can be retrieved */
+static const char unknown_str[] = "n/a";
+
+/* maximum output string length */
+#define MAXLEN 2048
+
+/* output formats */
+static const struct arg args[] = {
+	/* function, format, argument */
+  // { netspeed_rx, "%sB/s 󰖩 ", "wlp3s0" },
+	// { run_command, "%s    ", "find /home/ak/.mail/Inbox/new -type f -printf . | wc -c" },
+  { cpu_perc, " %s% 󱓞 ", "NULL" },
+  { ram_used,   "%s ", "NULL" },
+  { temp,   "| %s󰔄 ",  "/sys/class/thermal/thermal_zone0/temp" },
+	{ wifi_essid, "| %s 󰖩 ", "wlp3s0" },
+	//{ battery_state, "| %s ", "BAT0" },
+	//{ battery_perc, "| %3s%%  ", "BAT0" },
+  //{ battery_remaining, "| %s ", "BAT0" },
+  {run_command, "| %s " , "cat /sys/class/power_supply/BAT0/capacity" },
+	{ datetime, "| %s", "%a %d %H:%M " },
+};
